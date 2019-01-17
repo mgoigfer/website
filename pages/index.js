2991dadd006361ee1a8f1580943c89fa969ca5f1
Mikel Goig <m@mikelgoig.com>
2019-01-17 11:12:03 +0100

    Add Tailwind CSS framework

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import '../styles/index.css';
+
 export default class extends React.Component {
   render() {
     return (
-      <div>
-        <h1>¡Hola Mundo!</h1>
-        <p>Bienvenidos al curso de Next.js</p>
+      <div className="p-4 shadow rounded bg-white">
+        <h1 className="text-purple leading-normal">
+          Next.js
+        </h1>
+
+        <p className="text-grey-dark">
+          with Tailwind CSS
+        </p>
       </div>
     );
   }
