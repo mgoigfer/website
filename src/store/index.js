2244ac3d72cc17ebd5e3a721939638e7d269ca31
Mikel Goig <m@mikelgoig.com>
2019-02-11 13:29:45 +0000

    [CLP]

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,10 @@ import thunkMiddleware from 'redux-thunk';
 /* Reducer */
 import reducer, { exampleInitialState } from 'reducers';
 
-export default (initialState = exampleInitialState) => createStore(
+const makeStore = (initialState = exampleInitialState) => createStore(
   reducer,
   initialState,
   composeWithDevTools(applyMiddleware(thunkMiddleware)),
 );
+
+export default makeStore;
